cda7cab31ee5c8c2042783e442dedb0548e71c41
sreeharsha2736 <sreeharsha.guntamadugu@gmail.com>
2024-07-24 18:57:05 -0400

    cart page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { Button, Container, Nav, Navbar as NavbarBS } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
 const Navbar = () => {
+    const {openCart, cartQuantity} = useShoppingCart()
     return (
         <NavbarBS className="bg-white shadow-sm mb-3">
             <Container>
@@ -16,14 +18,16 @@ const Navbar = () => {
                         About
                     </Nav.Link>
                 </Nav>
-                <Button style={{ width: "3rem", height: "3rem" }} variant="outline-primary" className="rounded-circle">
+                {cartQuantity >0 &&(
+                <Button onClick={openCart} style={{ width: "3rem", height: "3rem" }} variant="outline-primary" className="rounded-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                     </svg>
                     <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-                        style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", transform: "translate(25%, 5%)" }}>3
+                        style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", transform: "translate(25%, 5%)" }}>
+                            {cartQuantity}
                     </div>
-                </Button>
+                </Button>)}
             </Container>
         </NavbarBS>
 
